b65e723be162fe0cad1b8ce8ddff12927db7b483
PANDASANG1231 <zwj63518583@gmail.com>
2022-02-17 14:04:58 -0800

    Debug Procfile

--- app/app.py
+++ app/app.py
@@ -8,10 +8,10 @@ import dash_bootstrap_components as dbc
 import dash_alternative_viz as dav
 from dash.dependencies import Input, Output
 
-# import sys
-# sys.path.append("./app")
-from .layout import *
-from .plot import *
+import sys
+sys.path.append("./app")
+from layout import *
+from plot import *
 
 app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP, '/css/button.css'])
 
